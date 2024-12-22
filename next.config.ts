cae8812f41c4a22be52add5f481210b82c5e1959
Penk <pongparn.c@deftdev.tech>
2024-12-22 13:39:18 +0700

    set up lint and prettier

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // eslint: {
+  //   dirs: ['pages', 'utils','app'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  // },
+
   /* config options here */
 };
 
